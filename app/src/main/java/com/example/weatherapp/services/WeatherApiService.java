e0f716bbb774ebd4966d70219cdf8bb5db6795f9
DaV <DaV@DESKTOP-GH660AU>
2024-01-27 13:35:44 +0100

    Added function allowing get 5-day forecast

--- app/src/main/java/com/example/weatherapp/services/WeatherApiService.java
+++ app/src/main/java/com/example/weatherapp/services/WeatherApiService.java
@@ -1,7 +1,10 @@
 package com.example.weatherapp.services;
 
 import com.example.weatherapp.models.CurrentWeather;
+import com.example.weatherapp.models.LongTermForecast;
+
 import java.io.IOException;
+import java.nio.DoubleBuffer;
 import java.util.concurrent.CompletableFuture;
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -44,6 +47,48 @@ public class WeatherApiService {
         return currentWeatherData[0];
     }
 
+    public CurrentWeather getWeatherByCoordinates(double lat,double lon, String units) {
+        final CurrentWeather[] currentWeatherData = new CurrentWeather[1];
+
+        CompletableFuture<CurrentWeather> futureWeather = CompletableFuture.supplyAsync(() -> {
+            try {
+                return weatherApiInterface.getWeatherByCoordinates(lat,lon, API_KEY, units).execute().body();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        futureWeather.thenAccept(currentWeatherModel -> currentWeatherData[0] = currentWeatherModel)
+                .exceptionally(throwable -> {
+                    throwable.printStackTrace();
+                    return null;
+                });
+
+        futureWeather.join();
+        return currentWeatherData[0];
+    }
+
+    public LongTermForecast getForecastByCityName(String cityName, String units) {
+        final LongTermForecast[] longTermForecastData = new LongTermForecast[1];
+
+        CompletableFuture<LongTermForecast> futureWeather = CompletableFuture.supplyAsync(() -> {
+            try {
+                return weatherApiInterface.getForecastByCity(cityName, API_KEY, units).execute().body();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        futureWeather.thenAccept(currentWeatherModel -> longTermForecastData[0] = currentWeatherModel)
+                .exceptionally(throwable -> {
+                    throwable.printStackTrace();
+                    return null;
+                });
+
+        futureWeather.join();
+        return longTermForecastData[0];
+    }
+
     public interface WeatherApiInterface {
         @GET("weather")
         Call<CurrentWeather> getWeatherByCity(
@@ -56,6 +101,12 @@ public class WeatherApiService {
         Call<CurrentWeather> getWeatherByCoordinates(
                 @Query("lat") double latitude,
                 @Query("lon") double longitude,
+                @Query("appid") String apiKey,
+                @Query("units") String units
+        );
+        @GET("forecast")
+        Call<LongTermForecast> getForecastByCity(
+                @Query("q") String cityName,
                 @Query("appid") String apiKey,
                 @Query("units") String units
         );
